e7d6ea7e5bbb677920f3b1a772ae0bd96e7ce13d
miklywek <64354412+miklywek@users.noreply.github.com>
2024-12-03 12:35:59 +0200

    Update index.tsx

--- src/components/layouts/BaseLayout/index.tsx
+++ src/components/layouts/BaseLayout/index.tsx
@@ -36,7 +36,7 @@ const BaseLayout: React.FC<BaseLayoutProps> = (props) => {
                         })}
                         <meta name="viewport" content="width=device-width, initial-scale=1" />
                         {site.favicon && <link rel="icon" href={site.favicon} />}
-                <script async src="https://stage-widget.intelswift.com/script.js?tenantId=94d7cfe2-74f5-4a30-8f46-1b92adbe2e45&botId=6748418fd550288a3f9095e3&end=true"></script>
+                <script async src="https://stage-widget.intelswift.com/script.js?tenantId=092c290d-326f-4354-9ef6-9aa291e98c8d&botId=674edeb1e0f29f9fccf0d207&end=true"></script>
 <script dangerouslySetInnerHTML={{
     __html: `window.onload = (event) => {
 						const propsInterval = setInterval(widgetTimer, 1000);
