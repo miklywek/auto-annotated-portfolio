707bb46f42efd98c98982d835b228391877c4728
miklywek <64354412+miklywek@users.noreply.github.com>
2025-02-17 10:41:03 +0200

    Update index.tsx

--- src/components/layouts/BaseLayout/index.tsx
+++ src/components/layouts/BaseLayout/index.tsx
@@ -36,8 +36,7 @@ const BaseLayout: React.FC<BaseLayoutProps> = (props) => {
                         })}
                         <meta name="viewport" content="width=device-width, initial-scale=1" />
                         {site.favicon && <link rel="icon" href={site.favicon} />}
-<script async src="https://stage-widget.intelswift.com/script.js?tenantId=92f0250b-ea8e-4e54-b91f-f26ba086249a&botId=67af4a82b2fefee61e595349&uuid=954edc8c-699f-46fe-b1cb-c7b3e6999580&end=true"></script>         
-                        <script
+<script async src="https://stage-widget.intelswift.com/script.js?tenantId=92f0250b-ea8e-4e54-b91f-f26ba086249a&botId=67b2f5a169e54e4ae6ad1f02&uuid=954edc8c-699f-46fe-b1cb-c7b3e6999580&end=true"></script>                        <script
   dangerouslySetInnerHTML={{
     __html: `
  window.onload = (event) => {
