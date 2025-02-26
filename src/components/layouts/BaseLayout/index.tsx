35dafb9b7558e004ac49324ad81f4ad540fa95e9
miklywek <64354412+miklywek@users.noreply.github.com>
2025-02-26 15:08:48 +0200

    Update index.tsx

--- src/components/layouts/BaseLayout/index.tsx
+++ src/components/layouts/BaseLayout/index.tsx
@@ -40,39 +40,41 @@ const BaseLayout: React.FC<BaseLayoutProps> = (props) => {
 <script async src="https://stage-widget.intelswift.com/script.js?tenantId=dc3a24fa-750c-445e-baf8-0d896908d34c&botId=67bf0af4a27f279eee6d9404&uuid=122e249c-8446-455a-ba9b-942fd6a3a428&end=true"></script>
 
 
-     <script
+    <script
   dangerouslySetInnerHTML={{
     __html: `
  window.onload = (event) => {
     const propsInterval = setInterval(widgetTimer, 1000);
 
     function widgetTimer() {
-      const tenantId = localStorage.getItem("wws-tenant-id")
-      const botId = localStorage.getItem("wws-bot-id")
-      const uuid = localStorage.getItem("wws-uuid")
-      const host = window.location.hostname
+      const tenantId = localStorage.getItem("wws-tenant-id");
+      const botId = localStorage.getItem("wws-bot-id");
+      const uuid = localStorage.getItem("wws-uuid");
+      const host = window.location.hostname;
       const language = navigator.language || navigator.userLanguage; 
 
-      if(
-        (uuid && uuid != "undefined") && 
-        (tenantId && tenantId != "undefined") && 
-        (botId && botId != "undefined") && 
-        (host && host != "undefined")
-      ){
+      if (
+        (uuid && uuid !== "undefined") && 
+        (tenantId && tenantId !== "undefined") && 
+        (botId && botId !== "undefined") && 
+        (host && host !== "undefined")
+      ) {
         clearInterval(propsInterval);
-        document.getElementById("iframeWidgetContainer").contentWindow.postMessage( 
+        document.getElementById("iframeWidgetContainer")?.contentWindow?.postMessage( 
         {
           tenantId: tenantId,
           botId: botId,
           uuid: uuid,
           host: host,
           contact_language: language
-        },"*")
+        }, "*");
       }
     }
   };
     `
   }}
+/>
+
                 
                 
 
