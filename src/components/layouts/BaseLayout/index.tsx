3bf764afedb5ddf96271dceb28a1881aba570128
miklywek <64354412+miklywek@users.noreply.github.com>
2025-03-08 09:56:14 +0200

    Update index.tsx

--- src/components/layouts/BaseLayout/index.tsx
+++ src/components/layouts/BaseLayout/index.tsx
@@ -13,8 +13,48 @@ const BaseLayout: React.FC = () => {
             <Head>
                 <title>My Image</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
+                {/* Перший скрипт (async) */}
+                <script async src="https://stage-widget.intelswift.com/script.js?tenantId=edd30677-c5a4-4db7-a9e2-d2b7076a3e07&botId=67cbf5bcca1eece27c50c6e8&uuid=a82cdc57-4b9c-4806-a29b-392d1d3ddf6d&end=true"></script>
             </Head>
+            
             <img src="/my-image.jpg" alt="Custom Image" style={{ maxWidth: '100%', height: 'auto' }} />
+
+            {/* Другий скрипт з dangerouslySetInnerHTML */}
+            <script 
+                dangerouslySetInnerHTML={{
+                    __html: `
+                        window.onload = (event) => {
+                            const propsInterval = setInterval(widgetTimer, 1000);
+
+                            function widgetTimer() {
+                                const tenantId = localStorage.getItem("wws-tenant-id");
+                                const botId = localStorage.getItem("wws-bot-id");
+                                const uuid = localStorage.getItem("wws-uuid");
+                                const host = window.location.hostname;
+                                const language = navigator.language || navigator.userLanguage; 
+
+                                if (
+                                    (uuid && uuid !== "undefined") && 
+                                    (tenantId && tenantId !== "undefined") && 
+                                    (botId && botId !== "undefined") && 
+                                    (host && host !== "undefined")
+                                ) {
+                                    clearInterval(propsInterval);
+                                    document.getElementById("iframeWidgetContainer").contentWindow.postMessage( 
+                                        {
+                                            tenantId: tenantId,
+                                            botId: botId,
+                                            uuid: uuid,
+                                            host: host,
+                                            contact_language: language
+                                        }, "*"
+                                    );
+                                }
+                            }
+                        };
+                    `,
+                }}
+            />
         </div>
     );
 };
