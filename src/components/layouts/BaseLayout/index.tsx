48e715d4f256e059ec3acafd4280aa5acff3d063
miklywek <64354412+miklywek@users.noreply.github.com>
2025-03-09 21:14:17 +0200

    Update index.tsx

--- src/components/layouts/BaseLayout/index.tsx
+++ src/components/layouts/BaseLayout/index.tsx
@@ -14,15 +14,14 @@ const BaseLayout: React.FC = () => {
                 <title>My Image</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 {/* Перший скрипт (async) */}
-<script async src="https://stage-widget.intelswift.com/script.js?tenantId=edd30677-c5a4-4db7-a9e2-d2b7076a3e07&botId=67cc1f00ca1eece27c50d824&uuid=a82cdc57-4b9c-4806-a29b-392d1d3ddf6d&end=true"></script>            </Head>
-            
+<script async src="https://stage-widget.intelswift.com/script.js?tenantId=da996429-eb7b-4f85-b7d7-b49b040212c6&botId=67cde81a8621e3f1114abb88&uuid=6bfaf9db-4ad5-47b0-9862-901fd0b90338&end=true"></script>            
             <img src="/my-image.jpg" alt="Custom Image" style={{ maxWidth: '100%', height: 'auto' }} />
 
             {/* Другий скрипт з dangerouslySetInnerHTML */}
             <script 
                 dangerouslySetInnerHTML={{
                     __html: `
-                       window.onload = (event) => {
+                     window.onload = (event) => {
     const propsInterval = setInterval(widgetTimer, 1000);
 
     function widgetTimer() {
